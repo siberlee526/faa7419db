4a61b6c32613bf4509001ec9ec615887c46d8355
siberlee526 <siberlee526@users.noreply.github.com>
2016-02-01 17:19:41 +0800

    modify exact function scripts

--- code/codeacad/tools/exact_codegraph_x64.bat
+++ code/codeacad/tools/exact_codegraph_x64.bat
@@ -2,9 +2,13 @@
 echo 复制文件中，请稍等......
 set srcpath=G:\githubcodelibrary\code\codeacad\bin
 set dirpath=C:\Users\Administrator\Desktop\codegraghoutput
-xcopy /e %srcpath%\config %dirpath%\
+
 xcopy /y %srcpath%\acadloader.dll %dirpath%
 xcopy /y %srcpath%\CodeGraph.exe %dirpath%
 xcopy /y %srcpath%\readme %dirpath%
+
+md %dirpath%\config
+xcopy /y /E %srcpath%\config %dirpath%\config
+
 echo 复制文件已完成！按任意键退出本窗口
 pause
